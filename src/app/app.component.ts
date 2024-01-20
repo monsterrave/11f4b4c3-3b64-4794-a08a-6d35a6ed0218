73964c59b2dc0df3b43623317fd947ac6cdfe70c
monsterrave <benja.glanz@gmail.com>
2024-01-20 20:53:59 +0100

    Init

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.sass'
+})
+export class AppComponent {
+  title = '11f4b4c3-3b64-4794-a08a-6d35a6ed0218';
+}
